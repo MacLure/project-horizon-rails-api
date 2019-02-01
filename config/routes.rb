44351eb92750483ecb274e868a69fa15c7aeeab5
Gabriela <gab.jo.cuello@gmail.com>
2019-01-31 23:34:41 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,35 @@
 Rails.application.routes.draw do
-  post 'student_token' => 'student_token#create'
-  post 'admin_token' => 'admin_token#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  match '*path', :controller => 'application', :action => 'handle_options_request', :constraints => {:method => 'OPTIONS'}, via: [:options]
+
+  namespace :admin do
+     get '/' => 'dashboard#index'
+     post 'admin_token' => 'admin_token#create'
+     get 'admin/current' => 'admins#current'
+     post 'find_admin' => 'admins#find'
+
+
+
+     resources :students
+     resources :cohorts
+     resources :admins
+     resources :assignemnts
+     resources :events
+     resources :contacts
+     resources :companies
+
+
+   end
+
+   namespace :student do
+     get '/' => 'dashboard#index'
+     post 'student_token' => 'student_token#create'
+     get 'student/current' => 'students#current'
+    post 'find_student' => 'students#find'
+
+     resources :students
+     resources :cohorts
+     resources :events
+     resources :assignments
+
+   end
+end 
